ccbddb0ae09764f50561051cf37061ee8e5d73f7
Sattanathan Subramanian <sat@Sattanathans-MacBook-Pro-2.local>
2018-11-01 23:43:25 +0100

    Number of digits got fixed

--- src/Counter.js
+++ src/Counter.js
@@ -220,7 +220,6 @@ function Counter() {
             <option value={2}>2</option>
             <option value={3}>3</option>
             <option value={4}>4</option>
-            <option value={5}>5</option>
           </Select>
       </FormControl>    
 
